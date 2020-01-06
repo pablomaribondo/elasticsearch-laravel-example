79fed0ef8e9d429d13a748402444f4cd707f032b
Pablo Maribondo <pablomgvieira@gmail.com>
2020-01-06 14:51:01 -0300

    Elasticsearch integration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use App\Repositories\Articles;
+use Elasticsearch\Client;
+use Elasticsearch\ClientBuilder;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,10 +16,29 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind(
-            Articles\ArticlesRepository::class,
-            Articles\EloquentRepository::class
-        );
+        $this->app->bind(Articles\ArticlesRepository::class, function ($app) {
+            // This is useful in case we want to turn-off our
+            // search cluster or when deploying the search
+            // to a live, running application at first.
+            if (! config('services.search.enabled')) {
+                return new Articles\EloquentRepository();
+            }
+
+            return new Articles\ElasticsearchRepository(
+                $app->make(Client::class)
+            );
+        });
+
+        $this->bindSearchClient();
+    }
+
+    private function bindSearchClient()
+    {
+        $this->app->bind(Client::class, function ($app) {
+            return ClientBuilder::create()
+                ->setHosts($app['config']->get('services.search.hosts'))
+                ->build();
+        });
     }
 
     /**
